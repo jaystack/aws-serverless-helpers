609bbea0ec3f3fc08922a3317de5ae5daae6b37f
Miklos Suveges <miklos.suveges@gmail.com>
2020-08-07 21:34:14 +0200

    feat: composeDecoratedHandler

--- packages/core/src/lambda-middleware.ts
+++ packages/core/src/lambda-middleware.ts
@@ -3,6 +3,21 @@ import { Context, Callback, Handler } from "aws-lambda";
 export type DecoratedHandler<Event = any, Result = any> = (
   event: Event,
   context: Context,
-  dependencies: any,
-  callback: Callback<Result>
-) => Promise<any>;
+  dependencies?: any,
+  callback?: Callback<Result>
+) => Promise<Result> | void;
+
+export type DecoratorMiddleware = (handler: DecoratedHandler) => DecoratedHandler;
+
+export const composeDecoratorMiddlewares = (...middlewares: DecoratorMiddleware[]): DecoratorMiddleware => (
+  decoratedHandler: DecoratedHandler
+) => {
+  const decoratorChain = middlewares.reduceRight((current, next) => next(current), decoratedHandler);
+
+  return async (event, context, dependencies, callback) => {
+    return decoratorChain(event, context, dependencies, callback);
+  };
+};
+
+export const composeDecoratedHandler = (handler: DecoratedHandler, ...middlewares: DecoratorMiddleware[]): Handler =>
+  composeDecoratorMiddlewares(...middlewares)(handler);
